f38e98bba96f42811f1eae7d940a2f008963c133
zZLerisZz <yalerisatinet@gmail.com>
2022-12-17 18:16:41 +0300

    FirstCommit

--- src/Application.java
+++ src/Application.java
@@ -2,6 +2,6 @@ package src.ru.croc.IndividualProject;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Проверка гитхаба");
     }
 }
